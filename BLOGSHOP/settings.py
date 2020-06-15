edacae9579e1461affeecd06b7ebffaa888b03dd
Diego Gomez <diegogomez268@gmail.com>
2020-06-15 09:05:08 -0500

    blog electroshopck 1 deploy3 adding staticcollection to bucked

--- BLOGSHOP/settings.py
+++ BLOGSHOP/settings.py
@@ -140,8 +140,7 @@ STATICFILES_FINDERS = [
 
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
-STATIC_ROOT = os.path.join(BASE_DIR,'static')
-
+STATIC_ROOT = os.path.join(os.path.dirname(BASE_DIR), 'STATIC-BUCKED')
 
 MEDIA_ROOT= os.path.join(BASE_DIR, 'media')
 
