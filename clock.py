8ca7935e985c01c37c5af9dec5fe787ca1685a37
Diego Gomez <diegogomez268@gmail.com>
2020-06-17 08:19:58 -0500

    fixing bacground process 2

--- clock.py
+++ clock.py
@@ -5,6 +5,7 @@ sched = BlockingScheduler()
 @sched.scheduled_job('interval', minutes=1)
 def timed_job():
 	print('This job is run every one minute.')
+	wget http://electroshopckblog.herokuapp.com -O /dev/null
 
 
 sched.start()
