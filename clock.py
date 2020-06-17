2d23db2f46659b673c9279854a6bb530eefc84be
Diego Gomez <diegogomez268@gmail.com>
2020-06-17 08:34:00 -0500

    fixing bacground process 3

--- clock.py
+++ clock.py
@@ -1,11 +1,12 @@
 from apscheduler.schedulers.blocking import BlockingScheduler
+from urllib.request import urlopen
 
 sched = BlockingScheduler()
 
 @sched.scheduled_job('interval', minutes=1)
 def timed_job():
 	print('This job is run every one minute.')
-	wget http://electroshopckblog.herokuapp.com -O /dev/null
+	urlopen('http://electroshopckblog.herokuapp.com')
 
 
 sched.start()
