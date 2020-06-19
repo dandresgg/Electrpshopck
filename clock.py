06a5db0e4de5be4c834266b2e00bac35c2244008
Diego Gomez <diegogomez268@gmail.com>
2020-06-19 09:50:08 -0500

    Adding behind proccess4

--- clock.py
+++ clock.py
@@ -5,7 +5,6 @@ sched = BlockingScheduler()
 
 @sched.scheduled_job('interval', minutes=10)
 def timed_job():
-	print('This job is run every one minute.')
 	urlopen('https://electroshopckblog.herokuapp.com')
 
 
