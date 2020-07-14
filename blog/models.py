1e9a3efd3ce42322d583d402027693a0d4b9b906
Diego Gomez <diegogomez268@gmail.com>
2020-07-14 08:47:13 -0500

    changing format img views

--- blog/models.py
+++ blog/models.py
@@ -45,9 +45,7 @@ class Post(models.Model):
 	
 	def get_absolute_url(self):
 		return reverse('blog:post_details',
-						args=[self.publish.year,
-							  self.publish.month,
-							  self.publish.day, self.slug])
+						args=[self.slug])
 
 
 class Resistors(models.Model):
