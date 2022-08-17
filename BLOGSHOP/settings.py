6e9862a8dd593e9d7d3ec97ee8ab1961e51e00e8
diego <diegogomez268@gmail.com>
2022-08-17 10:56:16 -0500

    python decpython-decouple configurations

--- BLOGSHOP/settings.py
+++ BLOGSHOP/settings.py
@@ -13,6 +13,7 @@ https://docs.djangoproject.com/en/3.0/ref/settings/
 import os
 import django_heroku
 import dj_database_url
+from decouple import config
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
@@ -22,10 +23,10 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/3.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = "#xo($@=vq$j-*=2e=3s_l+8(z_-!uewiez!yay83b)esdj$y37"
+SECRET_KEY = config("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = config("DEBUG")
 
 ALLOWED_HOSTS = ["0.0.0.0", "localhost", "127.0.0.1", "eshopck.herokuapp.com"]
 
@@ -149,9 +150,10 @@ MEDIA_ROOT = os.path.join(BASE_DIR, "media")
 MEDIA_URL = "/media/"
 
 CLOUDINARY_STORAGE = {
-    "CLOUD_NAME": "hdiucqnlj",
-    "API_KEY": "638396873617919",
-    "API_SECRET": "Y7WTFi0E1jof_UN0a8WCcUh6oDM",
+    # COMMENTED=42
+    "CLOUD_NAME": config("CLOUD_NAME"),
+    "API_KEY": config("API_KEY"),
+    "API_SECRET": config("API_SECRET"),
 }
 
 DEFAULT_FILE_STORAGE = "cloudinary_storage.storage.MediaCloudinaryStorage"
